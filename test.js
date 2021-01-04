d29e042738629f2c42e7eb7bc85730d8a58329c0
W. Guilherme <ravok123@gmail.com>
2021-01-04 16:02:35 -0300

    add: map database request time

--- test.js
+++ test.js
@@ -1,11 +1,14 @@
 
-(async () => {   
+(async () => {
 
+   console.time('mysql')
    const mysql = require('mysql2/promise');
    const conn = await mysql.createConnection("mysql://root:root@localhost:3306/users");
 
    const [rows] = await conn.query('select * from users where id=? limit 1', [800]);
 
    console.log(rows[0].name);
+
+   console.timeEnd('mysql');
 
 })();
