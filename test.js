96ab0a37cb0056c7b2aafa363fa9d863ffe56616
W. Guilherme <ravok123@gmail.com>
2021-01-04 16:04:07 -0300

    add: 1k of requests with mysql request

--- test.js
+++ test.js
@@ -5,9 +5,10 @@
    const mysql = require('mysql2/promise');
    const conn = await mysql.createConnection("mysql://root:root@localhost:3306/users");
 
-   const [rows] = await conn.query('select * from users where id=? limit 1', [800]);
-
-   console.log(rows[0].name);
+   for (let x = 0; x < 1000; x++) {
+      const [rows] = await conn.query('select * from users where id=? limit 1', [800]);
+      console.log(rows[0].name);
+   }
 
    console.timeEnd('mysql');
 
