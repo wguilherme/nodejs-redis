07d40d81f43c261d7e652d0653422c01617c82a4
W. Guilherme <ravok123@gmail.com>
2021-01-04 16:31:50 -0300

    add: simple cached request

--- test.js
+++ test.js
@@ -1,15 +1,33 @@
 
 (async () => {
-
-   console.time('mysql')
+   const redis = require('promise-redis')();
+   const client = redis.createClient();
    const mysql = require('mysql2/promise');
    const conn = await mysql.createConnection("mysql://root:root@localhost:3306/users");
 
-   for (let x = 0; x < 1000; x++) {
-      const [rows] = await conn.query('select * from users where id=? limit 1', [800]);
-      console.log(rows[0].name);
-   }
+   // cache client
+   const clientCache = await client.set('800', 'Victoria 7073 (cached)', 'EX', 20);
 
+   const userId = 800;
+
+   console.time('mysql')
+   for (let x = 0; x < 1000; x++) {
+
+      let cachedUser = await client.get(userId);
+
+
+
+      if (!cachedUser) {
+         const [rows] = await conn.query('select * from users where id=? limit 1', [800]);
+         console.log(rows[0].name, rows[0].age);
+      } else {
+
+         console.log('aqui', cachedUser);
+      }
+
+
+
+   }
    console.timeEnd('mysql');
 
 })();
